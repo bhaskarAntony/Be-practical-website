45f6bcd358ac636cc9efcfcd6327bcb9878ce3aa
bhaskarAntonyCoder <codercss3958@gmail.com>
2023-09-13 04:15:20 +0530

    all be practical designed files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -64,6 +64,59 @@ animatedItems.forEach((item) => {
             </div>
           </div>
         </div>
+        <div className="home-companies">
+          <div>
+            <ul className="homescroll1">
+              <li><h3>CodeOps</h3></li>
+              <li><h3>AppScript</h3></li>
+              <li><h3>Eminds</h3></li>
+              <li><h3>Henkel</h3></li>
+              <li><h3>MRK</h3></li>
+              <li><h3>BlueTree</h3></li>
+              <li><h3>InfoMaze</h3></li>
+              <li><h3>BlueTechnoSoft</h3></li>
+              <li><h3>IdexPoke</h3></li>
+              <li><h3>AMBC</h3></li>
+              <li><h3>DigitalShark</h3></li>
+              <li><h3>InnoMinds</h3></li>
+              <li><h3>CurveWing</h3></li>
+              <li><h3>Endexel</h3></li>
+              <li><h3>Joules to Watts</h3></li>
+              <li><h3>Entoss</h3></li>
+              <li><h3>CareAtor</h3></li>
+              <li><h3>Fartune Softech</h3></li>
+              <li><h3>Date Template</h3></li>
+              <li><h3>ExamRoom</h3></li>
+              <li><h3>DiaToz</h3></li>
+            </ul>
+          </div>
+          <div >
+          <ul className="homescroll2">
+              <li><h3>CodeOps</h3></li>
+              <li><h3>AppScript</h3></li>
+              <li><h3>Eminds</h3></li>
+              <li><h3>Henkel</h3></li>
+              <li><h3>MRK</h3></li>
+              <li><h3>BlueTree</h3></li>
+              <li><h3>InfoMaze</h3></li>
+              <li><h3>BlueTechnoSoft</h3></li>
+              <li><h3>IdexPoke</h3></li>
+              <li><h3>AMBC</h3></li>
+              <li><h3>DigitalShark</h3></li>
+              <li><h3>InnoMinds</h3></li>
+              <li><h3>CurveWing</h3></li>
+              <li><h3>Endexel</h3></li>
+              <li><h3>Joules to Watts</h3></li>
+              <li><h3>Entoss</h3></li>
+              <li><h3>CareAtor</h3></li>
+              <li><h3>Fartune Softech</h3></li>
+              <li><h3>Date Template</h3></li>
+              <li><h3>ExamRoom</h3></li>
+              <li><h3>DiaToz</h3></li>
+              <li><h3>KPSS Kalpar</h3></li>
+            </ul>
+          </div>
+        </div>
     </div>
   )
 }
