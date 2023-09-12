3838663c35a5935bc5cd9c1dfe5cc3e81f5b4d84
bhaskarAntonyCoder <codercss3958@gmail.com>
2023-09-12 17:18:57 +0530

    all be practical designed files

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,8 +9,8 @@ import video3 from '../images/video3.mp4'
 
 function Skills() {
   return (
-    <section className='container-fluid skillsmain overflow-hidden'>
-      <div className="container skillsecond p-lg-3 pt-5">
+    <section className='container-fluid skillsmain overflow-hidden p-0'>
+      <div className=" skillsecond pt-5">
         <div className="row">
             <div className="col-12 col-md-12 col-lg-6">
                 <div className="left col-12 col-md-12 col-lg-10">
@@ -23,6 +23,7 @@ function Skills() {
                     <h1>Why You Choose? Be-Practical</h1>
                     <p className='mt-3'>We are giving unloimited access to learn best courses from best trainers to learn hundreds of practical lessions ins multitude fields.</p>
                     <p></p>
+                    <div className="status">
                     <div className="row video-status d-flex g-2">
                         <div className="col-3 col-sm-6 col-md-6 col-lg-3 mb-2">
                        <div className="video">
@@ -44,6 +45,7 @@ function Skills() {
                        <video src={video3} autoPlay muted loop></video>
                        </div>
                         </div>
+                    </div>
                     </div>
                    </div>
                 </div>
