a12edeb45e216befcd1a5bd29e0b3e6dcd5f6b1f
bhaskarAntonyCoder <codercss3958@gmail.com>
2023-09-14 19:03:59 +0530

    all be practical designed files

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -14,7 +14,7 @@ function Course() {
         </div>
         <div className="col-12 col-md-12 col-lg-6">
             <div className="course-right">
-                <div className="course-card">
+                <div className="course-card" data-aos="zoom-in-up">
                   <div className="course-card-curve"></div>
                  <div>
                  <h3 className='course-name'>Full-stack</h3>
@@ -22,27 +22,27 @@ function Course() {
                  </div>
                  <div className="row">
                   <div className="col-4">
-                   <div className="course-icon">
+                   <div className="course-icon" data-aos="zoom-in-down">
                    <img src="https://algoscript.in/assets/img/tools/mern/Mern.png" alt="" />
                    </div>
                   </div>
                   <div className="col-4">
-                   <div className="course-icon">
+                   <div className="course-icon" data-aos="zoom-in-down">
                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png" alt="" />
                    </div>
                   </div>
                   <div className="col-4">
-                   <div className="course-icon">
+                   <div className="course-icon"  data-aos="zoom-in-down">
                    <img src="https://icon-library.com/images/java-icon-png/java-icon-png-2.jpg" alt="" />
                    </div>
                   </div>
                  </div>
                 </div>
-                <div className="course-card">
+                <div className="course-card" data-aos="zoom-in-up">
                   <h3 className='course-name'>Cloud - computing</h3>
                   Be pacricle is software training institute in banglore that provide programs for people looking for cloud computing training 
                 </div>
-                <div className="course-card">
+                <div className="course-card" data-aos="zoom-in-up">
                   <h3 className='course-name'>Digital marketing</h3>
                   Be-practicle digital marketing course in banglire offer comparehensive curriculam,practical projects nad flexible learning options
                 </div>
